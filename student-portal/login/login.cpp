d6eb3d30e4a6aa66f9aa4c3b22ee7668c05bd0a9
HammadFaizv <hammadfaza12345@gmail.com>
2026-08-15 17:20:16 +0530

    Login added encryption

--- student-portal/login/login.cpp
+++ student-portal/login/login.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <string>
 
+std::string encrypt(std::string s) {
+    for(int i = 0; i<s; i++) {
+        s[i] = s[i] - 1;
+    }
+}
+
 bool login(std::string username, std::string password) {
     if (username == "admin" && password == "1234") {
         return true;
