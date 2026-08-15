c8aa74df930db54ef61b213a7a9b538d382e4975
HammadFaizv <hammadfaza12345@gmail.com>
2026-08-15 09:23:23 +0530

    Added define grade in academic.cpp

--- student-portal/academic.cpp
+++ student-portal/academic.cpp
@@ -4,6 +4,15 @@
 int mult(int a, int b) {
     return a * b;
 }
+int define_grade(int a) {
+    if (a >= 50) {
+        return 1;
+    } else if (a < 50 && a >= 30) {
+        return 2;
+    } else {
+        return 3;
+    }
+}
 
 void tell_score(int n) {
     std::cout << "Result score: , " << n << std::endl;
